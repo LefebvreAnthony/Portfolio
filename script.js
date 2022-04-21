b5eb020e841986fe29c946bcf8e5e6f79ce0fe3e
LefebvreAnthony <anthony@hotmail.fr>
2022-04-21 08:43:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const btn = document.querySelectorAll('button');
+const doorLeft = document.querySelector('.doors_left');
+doorLeft
+
+btn.forEach(element => {
+    element.addEventListener("click", (e) => {
+        e.preventDefault();
+
+    })
+})
